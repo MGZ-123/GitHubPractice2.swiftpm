9199c77f8638b32d87487769b021d37aba596ddc
Shawn Simon <Ssimon5338@stu.d214.org>
2025-04-11 13:21:30 -0500

    I made the variables and texfields.

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number1 = 0
+    @State var number2 = 0
+    @State var answer = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        TextField("Please enter #1", value: $number1, format: .number)
+        TextField("Please enter #2", value: $number2, format: .number)
     }
 }
